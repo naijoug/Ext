4c902948d53484786cd5f5e5c0a45d9b2d756ace
naijoug <naijoug@126.com>
2021-09-23 15:45:53 +0800

    add bundle extension

---
+++ Sources/Extension/Foundation/Bundle+Ext.swift
@@ -0,0 +1,25 @@
+//
+//  Bundle+Ext.swift
+//  Ext
+//
+//  Created by guojian on 2021/9/22.
+//
+
+import Foundation
+
+public extension ExtWrapper where Base == Bundle {
+    
+    static func bundle(for cls: AnyClass? = nil, bundleName: String) -> Bundle? {
+        var bundle = Bundle.main
+        if let cls = cls { bundle = Bundle(for: cls) }
+        let bundlePath = bundle.path(forResource: bundleName, ofType: "bundle")
+        Ext.debug("bundlePath: \(bundlePath ?? "")", locationEnabled: false)
+        guard let path = bundlePath else { return nil }
+        return Bundle(path: path)
+    }
+    
+    static func path(for cls: AnyClass? = nil, bundleName: String, fileName: String) -> String? {
+        return bundle(for: cls, bundleName: bundleName)?.path(forResource: fileName, ofType: nil)
+    }
+    
+}
---
+++ Sources/Feature/Animation.swift
@@ -0,0 +1,71 @@
+//
+//  Animation.swift
+//  Ext
+//
+//  Created by guojian on 2021/9/23.
+//
+
+import UIKit
+
+public enum Animation {}
+
+public extension Animation {
+    
+    /// 图片飞行动画
+    static func fly(imageNamed: String, startView: UIView?, endView: UIView?, handler: Ext.ResultVoidHandler?) {
+        guard let view = UIApplication.shared.ext.mainWindow,
+            let startView = startView, let endView = endView,
+            let startPoint = startView.superview?.convert(startView.center, to: view),
+            let endPoint = endView.superview?.convert(endView.center, to: view) else {
+            Ext.debug("fly animation path error.")
+            handler?(.failure(Ext.Error.inner("animation path error.")))
+            return
+        }
+        Ext.debug("\(startView) | \(endView)")
+        Ext.debug("animation start: \(startPoint) -> end: \(endPoint)")
+        
+        guard let image = UIImage(named: imageNamed) else {
+            handler?(.failure(Ext.Error.inner("fly image is nil.")))
+            return
+        }
+        
+        let animationImageView = UIImageView(image: image)
+        view.addSubview(animationImageView)
+        animationImageView.center = startPoint
+        
+        CATransaction.begin()
+        CATransaction.setCompletionBlock {
+            Ext.debug("fly animation completion.")
+            animationImageView.removeFromSuperview()
+            handler?(.success(()))
+        }
+        animationImageView.layer.add(flyAnimation(start: startPoint, end: endPoint), forKey: nil)
+        CATransaction.commit()
+    }
+    private static func flyAnimation(start: CGPoint, end: CGPoint) -> CAAnimationGroup {
+        let rotation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.toValue = Float.pi
+        let scale = CABasicAnimation(keyPath: "transform.scale")
+        scale.toValue = 0.2
+        let controlOffsetX: CGFloat = 100
+        let controlOffsetY: CGFloat = 100
+        let controlX = (start.x + end.x)/2 - controlOffsetX
+        let controlY = (start.y + end.y)/2 - controlOffsetY
+        
+        let keyframe = CAKeyframeAnimation(keyPath: "position")
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addQuadCurve(to: end, controlPoint: CGPoint(x: controlX, y: controlY))
+        keyframe.path = path.cgPath
+        keyframe.calculationMode = CAAnimationCalculationMode.paced
+        keyframe.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        
+        let groupAnimation = CAAnimationGroup()
+        groupAnimation.duration = 1.0
+        groupAnimation.fillMode = .forwards
+        groupAnimation.isRemovedOnCompletion = false
+        groupAnimation.animations = [rotation, scale, keyframe]
+        return groupAnimation
+    }
+    
+}
