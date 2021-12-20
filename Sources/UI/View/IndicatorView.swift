b30dcb68d2f1d05e3162ce4d9b85258870db21a9
guojian <guojian@hibee.com>
2021-12-20 18:36:22 +0800

    add indicator label

--- Sources/UI/View/IndicatorView.swift
+++ Sources/UI/View/IndicatorView.swift
@@ -1,16 +1,52 @@
 //
-//  IndicatorButton.swift
+//  IndicatorView.swift
 //  Ext
 //
-//  Created by naijoug on 2019/10/11.
+//  Created by guojian on 2021/12/20.
 //
 
-import UIKit
+import Foundation
 
 /// 指示器协议
 public protocol Indicatable: AnyObject {
     /// 指示器状态
     var isIndicating: Bool { get set }
+}
+
+/// 联网指示器 Label
+open class IndicatorLabel: UILabel, Indicatable {
+    
+    /// 指示器视图
+    open lazy var indicatorView: UIActivityIndicatorView = {
+        let indicatorView = UIActivityIndicatorView(style: .gray)
+        addSubview(indicatorView)
+        indicatorView.translatesAutoresizingMaskIntoConstraints = false
+        indicatorView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        indicatorView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        return indicatorView
+    }()
+    
+    /// 指示器状态
+    public var isIndicating: Bool = false {
+        didSet {
+            isIndicating ? indicatorView.startAnimating() : indicatorView.stopAnimating()
+        }
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        bringSubviewToFront(indicatorView)
+    }
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    @objc
+    open func setupUI() {}
+    
 }
 
 /// 联网指示器按钮
@@ -20,7 +56,6 @@ open class IndicatorButton: UIButton, Indicatable {
     open lazy var indicatorView: UIActivityIndicatorView = {
         let indicatorView = UIActivityIndicatorView(style: .gray)
         addSubview(indicatorView)
-        indicatorView.style = .gray
         indicatorView.translatesAutoresizingMaskIntoConstraints = false
         indicatorView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         indicatorView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
