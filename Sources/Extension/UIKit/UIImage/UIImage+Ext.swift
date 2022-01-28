f082df3a1d9e0a10a52983bc7f7ffa83deb973b8
naijoug <naijoug@126.com>
2022-01-28 18:56:38 +0800

    add UIImage extension

--- Sources/Extension/UIKit/UIImage/UIImage+Ext.swift
+++ Sources/Extension/UIKit/UIImage/UIImage+Ext.swift
@@ -139,27 +139,31 @@ public extension ExtWrapper where Base: UIImage {
     }
     
     /// 生成圆形图片
-    func toCircle() -> UIImage {
+    func toCircle() -> UIImage? {
         let size = base.size
-        //取最短边长
         let shotest = min(size.width, size.height)
-        //输出尺寸
         let outputRect = CGRect(x: 0, y: 0, width: shotest, height: shotest)
-        //开始图片处理上下文（由于输出的图不会进行缩放，所以缩放因子等于屏幕的scale即可）
         UIGraphicsBeginImageContextWithOptions(outputRect.size, false, 0)
         let context = UIGraphicsGetCurrentContext()!
-        //添加圆形裁剪区域
         context.addEllipse(in: outputRect)
         context.clip()
-        //绘制图片
         base.draw(in: CGRect(x: (shotest - size.width)/2,
                              y: (shotest - size.height)/2,
                              width: size.width,
                              height: size.height))
-        //获得处理后的图片
-        let maskedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        let maskedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return maskedImage
     }
     
+    /// 圆角
+    func roundedCorners(_ radius: CGFloat) -> UIImage? {
+        let size = base.size
+        UIGraphicsBeginImageContextWithOptions(size, false, base.scale)
+        defer { UIGraphicsEndImageContext() }
+        let rect = CGRect(origin: .zero, size: size)
+        UIBezierPath(roundedRect: rect, cornerRadius: radius).addClip()
+        base.draw(in: rect)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
 }
