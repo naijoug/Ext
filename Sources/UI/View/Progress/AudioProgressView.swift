4327a57f926eab7ab5c37002a2f5f6aa2d932435
naijoug <naijoug@126.com>
2022-03-15 19:05:57 +0800

    add progress view

---
+++ Sources/UI/View/Progress/AudioProgressView.swift
@@ -0,0 +1,70 @@
+//
+//  AudioProgressView.swift
+//  Ext
+//
+//  Created by naijoug on 2022/3/15.
+//
+
+import UIKit
+
+/// 音波进度视图
+public class AudioProgressView: ExtView {
+    
+    /// 进度槽颜色
+    public var trackColor: UIColor = .lightGray {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    /// 进度条颜色
+    public var progressColor: UIColor = .darkGray {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    /// 进度
+    public var progress: Float = 0.0 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    /// 音波数据
+    public var items = [Int]() {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    public override func setupUI() {
+        super.setupUI()
+        backgroundColor = .clear
+    }
+    
+    public override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let count = items.count
+        let max = items.max() ?? 0
+        guard count > 0, max > 0, rect.width > 0, rect.height > 0 else { return }
+        
+        let context = UIGraphicsGetCurrentContext()
+        
+        let itemW: CGFloat = rect.width / CGFloat(count)
+        let itemH: CGFloat = rect.height
+        let progressW = rect.width * CGFloat(progress)
+        for i in 0..<count {
+            let item = items[i]
+            let isZero = item == 0
+            let barH = isZero ? 0.5 : CGFloat(item)/CGFloat(max) * itemH
+            let barW = itemW * (isZero ? 1 : 0.8)
+            let barX = itemW * CGFloat(i) + (itemW - barW)/2
+            let barY = (itemH - barH) / 2
+            let barRect = CGRect(x: barX, y: barY, width: barW, height: barH)
+            //Ext.debug("bar rect: \(barRect) | \(count)")
+            let color = barX > progressW ? trackColor : progressColor
+            context?.setFillColor(color.cgColor)
+            context?.addRect(barRect)
+            context?.drawPath(using: .fill)
+        }
+    }
+}
--- Sources/UI/View/Progress/CircleProgressView.swift
+++ Sources/UI/View/Progress/CircleProgressView.swift
@@ -7,23 +7,29 @@
 
 import UIKit
 
-/// 进度条配置
-public struct ProgressConfig {
-    /// 进度条宽度
-    public var lineWidth: CGFloat = 2
-    /// 进度槽颜色
-    public var trackColor: UIColor = .lightGray
-    /// 进度条颜色
-    public var progressColor: UIColor = .darkGray
-}
-
 /// 圆形进度条
 open class CircleProgressView: ExtView {
     
-    /// 进度条配置
-    public var config: ProgressConfig = ProgressConfig()
+    /// 进度槽颜色
+    public var trackColor: UIColor = .lightGray {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    /// 进度条颜色
+    public var progressColor: UIColor = .darkGray {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    /// 进度条宽度
+    public var lineWidth: CGFloat = 2 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
     
-    /// 进度 [0 ~ 1.0]
+    /// 进度
     public private(set) var progress: CGFloat = 0.0 {
         didSet {
             if progress < 0 {
@@ -40,20 +46,20 @@ open class CircleProgressView: ExtView {
     open override func draw(_ rect: CGRect) {
         
         let path = UIBezierPath(arcCenter: CGPoint(x: bounds.midX, y: bounds.midY),
-                                radius: bounds.size.width/2 - config.lineWidth,
+                                radius: bounds.size.width/2 - lineWidth,
                                 startAngle: -CGFloat.pi/2,
                                 endAngle: CGFloat.pi*3/2,
                                 clockwise: true)
         
         trackLayer.fillColor = UIColor.clear.cgColor
-        trackLayer.strokeColor = config.trackColor.cgColor
-        trackLayer.lineWidth = config.lineWidth
+        trackLayer.strokeColor = trackColor.cgColor
+        trackLayer.lineWidth = lineWidth
         trackLayer.path = path.cgPath
         layer.insertSublayer(trackLayer, at: 0)
         
         progressLayer.fillColor = UIColor.clear.cgColor
-        progressLayer.strokeColor = config.progressColor.cgColor
-        progressLayer.lineWidth = config.lineWidth
+        progressLayer.strokeColor = progressColor.cgColor
+        progressLayer.lineWidth = lineWidth
         progressLayer.path = path.cgPath
         progressLayer.strokeStart = 0
         progressLayer.strokeEnd = CGFloat(progress)
